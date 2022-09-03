0b8afd8ad2f5965a3dd6aab8f31b718c29d0d8c3
james.jingqi.li <james.jingqi.li@gmail.com>
2022-09-03 10:02:18 -0700

    update

--- examples/linear_system_baseline.jl
+++ examples/linear_system_baseline.jl
@@ -8,28 +8,25 @@ using iLQGames:
 using Infiltrator
 using Optim
 using LinearAlgebra
+using Distributed
 include("diff_solver.jl")
 include("inverse_game_solver.jl")
 
 # parametes: number of states, number of inputs, sampling time, horizon
 nx, nu, ΔT, game_horizon = 4, 4, 0.1, 40
-
 # setup the dynamics
 struct LinearSystem <: ControlSystem{ΔT,nx,nu} end
 # state: (px, py, phi, v)
 dx(cs::LinearSystem, x, u, t) = SVector(u[1],u[2],u[3],u[4])
 dynamics = LinearSystem()
-
 # costs = (FunctionPlayerCost((g, x, u, t) -> (10*(x[1]-1)^2 + 0.1*(x[3]-pi/2)^2 + (x[4]-1)^2 + u[1]^2 + u[2]^2 - 0.1*((x[1]-x[5])^2 + (x[2]-x[6])^2))),
          # FunctionPlayerCost((g, x, u, t) -> ((x[5]-1)^2 + 0.1*(x[7]-pi/2)^2 + (x[8]-1)^2 + u[3]^2 + u[4]^2- 0.1*((x[1]-x[5])^2 + (x[2]-x[6])^2))))
 costs = (FunctionPlayerCost((g, x, u, t) -> ( 2*(x[3])^2 + 2*(x[4])^2 + u[1]^2 + u[2]^2)),
          FunctionPlayerCost((g, x, u, t) -> ( 2*(x[1]-x[3])^2 + 2*(x[2]-x[4])^2 + u[3]^2 + u[4]^2)))
-
 # indices of inputs that each player controls
 player_inputs = (SVector(1,2), SVector(3,4))
 # the horizon of the game
 g = GeneralGame(game_horizon, player_inputs, dynamics, costs)
-
 # get a solver, choose initial conditions and solve (in about 9 ms with AD)
 solver1 = iLQSolver(g, max_scale_backtrack=10, max_elwise_diff_step=Inf, equilibrium_type="OLNE_costate")
 x0 = SVector(0, 1, 1,1)
@@ -43,7 +40,6 @@ anim1 = @animate for i in 1:game_horizon
     plot!([0], seriestype = "vline", color = "black", label = "")
     plot!([1], seriestype = "vline", color = "black", label = "") 
 end
-
 gif(anim1, "LQ_OL.gif", fps = 10)
 
 
@@ -135,27 +131,27 @@ function inverse_game_gradient_descent(θ::Vector, g::GeneralGame, expert_traj::
     return θ_next, new_loss, gradient_value, equilibrium_type, new_traj, new_solver
 end
 
-max_GD_iteration_num =200
+# max_GD_iteration_num =200
 
-# θ = [9.0; 1.0; 2.0;1.0]
-θ = ones(8)
-θ_dim = length(θ)
-sol = [zeros(θ_dim) for iter in 1:max_GD_iteration_num+1]
-sol[1] = θ
-loss_values = zeros(max_GD_iteration_num+1)
-loss_values[1],_,_ = inverse_game_loss(sol[1], g, expert_traj1, x0, parameterized_cost, "OLNE_costate")
-gradient = [zeros(θ_dim) for iter in 1:max_GD_iteration_num]
-equilibrium_type = ["" for iter in 1:max_GD_iteration_num]
-traj_list = [zero(SystemTrajectory, g) for iter in 1:max_GD_iteration_num]
-solver_list = [[] for iter in 1:max_GD_iteration_num]
-for iter in 1:max_GD_iteration_num
-    sol[iter+1], loss_values[iter+1], gradient[iter], equilibrium_type[iter], _, _ = inverse_game_gradient_descent(sol[iter], 
-                                            g, expert_traj1, x0, 10, parameterized_cost, [],true)
-    println("Current solution: ", sol[iter+1])
-    if loss_values[iter+1]<0.1
-        break
-    end
-end
+# # θ = [9.0; 1.0; 2.0;1.0]
+# θ = ones(8)
+# θ_dim = length(θ)
+# sol = [zeros(θ_dim) for iter in 1:max_GD_iteration_num+1]
+# sol[1] = θ
+# loss_values = zeros(max_GD_iteration_num+1)
+# loss_values[1],_,_ = inverse_game_loss(sol[1], g, expert_traj1, x0, parameterized_cost, "OLNE_costate")
+# gradient = [zeros(θ_dim) for iter in 1:max_GD_iteration_num]
+# equilibrium_type = ["" for iter in 1:max_GD_iteration_num]
+# traj_list = [zero(SystemTrajectory, g) for iter in 1:max_GD_iteration_num]
+# solver_list = [[] for iter in 1:max_GD_iteration_num]
+# for iter in 1:max_GD_iteration_num
+#     sol[iter+1], loss_values[iter+1], gradient[iter], equilibrium_type[iter], _, _ = inverse_game_gradient_descent(sol[iter], 
+#                                             g, expert_traj1, x0, 10, parameterized_cost, [],true)
+#     println("Current solution: ", sol[iter+1])
+#     if loss_values[iter+1]<0.1
+#         break
+#     end
+# end
 
 #----------------------------------------------------------------------------------------------------------------------------------
 # max_GD_iteration_num = 40
@@ -223,7 +219,32 @@ function generalization_loss(g, θ, x0_set, expert_traj, parameterized_cost, equ
 end
 
 
-x0_set = [x0+0.1*rand(Normal(0,1),4) for ii in 1:20]
+function generate_traj(θ, x0_set, g, parameterized_cost, equilibrium_type_list )
+    n_data = length(x0_set)
+    conv = [false for ii in 1:n_data]
+    expert_traj_list = [zero(SystemTrajectory, g) for ii in 1:n_data]
+
+    for item in 1:n_data
+        if rand(1)[1]>0.5
+            equilibrium_type = equilibrium_type_list[1]
+        else
+            equilibrium_type = equilibrium_type_list[2]
+        end
+        solver = iLQSolver(g, max_scale_backtrack=10, max_elwise_diff_step=Inf, equilibrium_type=equilibrium_type)
+        conv[item], tmp, _ = solve(g, solver, x0_set[item])
+        for t in 1:g.h
+            expert_traj_list[item].x[t] = tmp.x[t]
+            expert_traj_list[item].u[t] = tmp.u[t]
+        end
+    end
+    return conv, expert_traj_list
+end
+
+
+
+x0_set = [x0+0.1*rand(Normal(0,1),4) for ii in 1:3]
+
+
 
 θ₀ = 2*ones(8)
 conv, rew, loss_lists, grad, equi, _ = test_experiments(g, θ₀, [x0], [expert_traj1], parameterized_cost,200)
@@ -234,8 +255,12 @@ iterations_BA = sum(equi[3][1][ii]!="" for ii in 1:length(equi[3][1]))
 
 generalization_loss(c)
 
-# test robustness to observation noise
+# 1. test robustness to observation noise
+#  X: 
+# Y1: state prediction loss.
+# Y2: generalization loss.    
 
+θ_true = [2.0;2.0;1.0;2.0;2.0;1.0;0.0;0.0]
 
 num_test = 20
 sampled_initial_states = [x0+[0.1;0.1;0.1;0.1; 0.1;0.1;0.1;0.1].*rand(Normal(0,1),nx) for ii in 1:num_test]
