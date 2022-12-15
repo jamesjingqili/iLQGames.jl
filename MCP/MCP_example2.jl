4c996c1d7289d154b995380dd2b7ca23d66f1f0f
james.jingqi.li <james.jingqi.li@gmail.com>
2022-12-15 02:37:36 -0800

    update MCP examples

--- MCP/MCP_example2.jl
+++ MCP/MCP_example2.jl
@@ -1,17 +1,19 @@
 using JuMP, PATHSolver
 
-M1 = [1 0; 0 1]
+# f_i(x) = 1/2*x'*M1*x + q1'*x
+# ∇f_1(x) = 2*x[1] + x[2] + q1[1]
+M1 = [1 0.5; 0.5 1]
 q1 = -[2;2]
 
-M2 = [1 0; 0 4]
+# ∇f_2(x) = 8*x[1] + x[1] + q2[1]
+M2 = [1 0.5; 0.5 4]
 q2 = -[3; 1]
 
 model = Model(PATHSolver.Optimizer)
 set_optimizer_attribute(model, "output", "no")
 @variable(model, x[1:2] >= 0)
-@constraint(model, M1 * x .+ q1 ⟂ x)
-@constraint(model, M2 * x .+ q2 ⟂ x)
-
+@constraint(model, 2*x[1]+x[2]+q1[1] ⟂ x[1])
+@constraint(model, 8*x[2]+q2[2] ⟂ x[2])
 optimize!(model)
 value.(x)
 solution_summary(model)
